874feff96950f456d9b065a4e080d7c4fe662e20
Umang Goel <umanggoel27@gmail.com>
2023-07-23 18:37:28 +0530

    trying to use .scss files

--- src/app/Form/page.tsx
+++ src/app/Form/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-// import Image from 'next/image'
+import styles from "./styles.scss";
 
 const Form: React.FC = () => {
   const [input1, setInput1] = useState("");
